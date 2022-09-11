176f80122ad9344a94c0b8ba0dbdc9ca47510e85
Bertel Schjerning <bertel.schjerning@econ.ku.dk>
2022-09-12 01:31:49 +0200

    Add excercises

---
+++ excercises/0_theory_tools/ex_ante/tools.py
@@ -0,0 +1,18 @@
+import numpy as np
+import math
+
+def gauss_hermite(n):
+
+    # a. calculations
+    i = np.arange(1,n)
+    a = np.sqrt(i/2)
+    CM = np.diag(a,1) + np.diag(a,-1)
+    L,V = np.linalg.eig(CM)
+    I = L.argsort()
+    V = V[:,I].T
+
+    # b. nodes and weights
+    x = L[I]
+    w = np.sqrt(math.pi)*V[:,0]**2
+
+    return x,w
